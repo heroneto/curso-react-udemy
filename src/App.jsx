77d6b0debb92bc24bc4ef67f337ab43453ff5f50
Heron Eto <hhqe@hotmail.com>
2024-07-15 20:54:52 -0300

    add usereducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
+
+function counterReducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1 < 0 ? state : state - 1
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [count, counterDispatch] = useReducer(counterReducer, 0)
 
-  function decrement() {
-    setCount((valor) => {
-      return valor - 1
-    })
-  }
+  // function decrement() {
+  //   setCount((valor) => {
+  //     return valor - 1
+  //   })
+  // }
 
-  function increment() {
-    setCount(10)
-  }
+  // function increment() {
+  //   setCount(10)
+  // }
 
   return (
     <>
       <h1>Contador: {count}</h1>
-      <button type='button' onClick={decrement}>Reduzir</button>
-      <button type='button' onClick={increment}>Aumentar</button>
+      <button type='button' onClick={() => counterDispatch({ type: 'decrement' })}>Reduzir</button>
+      <button type='button' onClick={() => counterDispatch({ type: 'increment' })}>Aumentar</button>
     </>
   )
 }
